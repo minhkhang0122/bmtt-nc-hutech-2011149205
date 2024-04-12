3b09f313031081358782ddd4590a21a9d3d0c36f
Minh Khang <minhkhang0122@gmail.com>
2024-04-12 09:35:14 +0700

    [add] lab02 Railfence

--- lab-02/api.py
+++ lab-02/api.py
@@ -1,11 +1,31 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere.vigenere_cipher import VigenereCipher
+from cipher.railfence.railfence_cipher import RailFenceCipher
 
 
 app = Flask(__name__)
 vigenere_Cipher = VigenereCipher()
 caesar_cipher = CaesarCipher()
+railfence_cipher = RailFenceCipher()
+
+
+#RailFence Algorithm
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 
 # Vigenere CIPHER ALGORITHM
