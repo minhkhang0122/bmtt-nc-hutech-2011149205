bcf6c7791dfa09c8b6884c350d91ab6d87ba8bad
Minh Khang <minhkhang0122@gmail.com>
2024-04-12 10:30:24 +0700

    [Add]Lab02 Transposition

--- lab-02/api.py
+++ lab-02/api.py
@@ -3,12 +3,33 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere.vigenere_cipher import VigenereCipher
 from cipher.railfence.railfence_cipher import RailFenceCipher
 from cipher.playfair.playfair_cipher import PlayFairCipher
+from cipher.transposition.transposition_cipher import TranspositionCipher
+
 
 app = Flask(__name__)
 vigenere_Cipher = VigenereCipher()
 caesar_cipher = CaesarCipher()
 railfence_cipher = RailFenceCipher()
 playfair_cipher = PlayFairCipher()
+transposition_cipher = TranspositionCipher()
+
+
+#Transposition API Call
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text,key)
+    return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypt_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypt_text})
+
 
 
 #PlayFair API Call
